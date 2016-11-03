aa35b8215fbbda29a705ef1eff7c8cdef5d79e7c
Jack Franklin <jack@jackfranklin.net>
2016-11-03 18:53:47 +0000

    Tidy up

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 
-import AppComponent from './App.vue'
+import AppComponent from './App/index.vue'
 
 const vm = new Vue({
   el: '#app',
   components: {
     app: AppComponent,
   },
-  render: h => h(AppComponent),
+  render: h => h('app'),
 })
